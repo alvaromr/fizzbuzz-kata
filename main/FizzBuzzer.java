99851d56e5052272ec8bf26262d01cb2b977cf41
alvaromr <alvaro.martin.rodriguez@alumnos.upm.es>
2015-11-04 00:23:01 +0100

    Remove subclases

--- main/FizzBuzzer.java
+++ main/FizzBuzzer.java
@@ -4,18 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FizzBuzzer {
+    public static final int FIZZ_NUMBER = 3;
+    public static final int BUZZ_NUMBER = 5;
+
     private List<Matcher> matchers;
     public FizzBuzzer() {
         matchers = new ArrayList<>();
-        matchers.add(new FizzMatcher());
-        matchers.add(new BuzzMatcher());
+        matchers.add(new Matcher(FIZZ_NUMBER, "Fizz"));
+        matchers.add(new Matcher(BUZZ_NUMBER, "Buzz"));
     }
 
     public String generate(int number) {
         String transformedNumber = "";
         for(Matcher matcher : matchers){
             if(matcher.matches(number)){
-                transformedNumber += matcher.appendTransformation(number);
+                transformedNumber += matcher.appendTransformation();
             }
         }
         return transformedNumber.isEmpty() ? String.valueOf(number) : transformedNumber;
--- main/Matcher.java
+++ main/Matcher.java
@@ -1,18 +1,22 @@
 package main;
 
-public abstract class Matcher {
+public class Matcher {
 
     private int numberToMatch;
+    private String transformation;
 
-    public Matcher(int numberToMatch) {
+    public Matcher(int numberToMatch, String transformation) {
         this.numberToMatch = numberToMatch;
+        this.transformation = transformation;
     }
 
-    public boolean matches(int number){
+    public boolean matches(int number) {
         return this.isDivisible(number, numberToMatch) || this.containsDigit(number, numberToMatch);
     }
 
-    public abstract String appendTransformation(int number);
+    public String appendTransformation() {
+        return this.transformation;
+    }
 
     protected boolean isDivisible(int number, int divisor) {
         return number % divisor == 0;
@@ -22,7 +26,7 @@ public abstract class Matcher {
         int currentNumber = number;
         int currentDigit = currentNumber % 10;
         boolean found = false;
-        while(currentDigit != 0 && !found){
+        while (currentDigit != 0 && !found) {
             found = currentDigit == digit;
             currentNumber /= 10;
             currentDigit = currentNumber % 10;
